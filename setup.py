16cfbfa5c8b547759142dcd353201ac39839e8d2
Arun Sharma <asharma@fb.com>
2018-10-31 14:37:32 -0700

    black

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 
-VERSION     = '1.0.0'
-DESCRIPTION = 'Implements the core Paxos algorithm as a set of composable classes'
+VERSION = "1.0.0"
+DESCRIPTION = "Implements the core Paxos algorithm as a set of composable classes"
 
 try:
     from setuptools import setup
@@ -10,22 +10,23 @@ except ImportError:
 
 
 setup(
-    name             = 'composable_paxos',
-    version          = VERSION,
-    description      = DESCRIPTION,
-    license          = "MIT",
-    long_description = "Implements the core Paxos algorithm as a set of composable classes",
-    url              = 'https://github.com/cocagne/python-composable-paxos',
-    author           = "Tom Cocagne",
-    author_email     = 'tom.cocagne@gmail.com',
-    provides         = ['composable_paxos'],
-    py_modules       = ['composable_paxos'],
-    keywords         = ['paxos'],
-    classifiers      = ['Development Status :: 5 - Production/Stable',
-                        'Intended Audience :: Developers',
-                        'License :: OSI Approved :: MIT License',
-                        'Programming Language :: Python',
-                        'Topic :: Software Development :: Libraries',
-                        'Topic :: System :: Networking'],
-    )
-
+    name="composable_paxos",
+    version=VERSION,
+    description=DESCRIPTION,
+    license="MIT",
+    long_description="Implements the core Paxos algorithm as a set of composable classes",
+    url="https://github.com/cocagne/python-composable-paxos",
+    author="Tom Cocagne",
+    author_email="tom.cocagne@gmail.com",
+    provides=["composable_paxos"],
+    py_modules=["composable_paxos"],
+    keywords=["paxos"],
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python",
+        "Topic :: Software Development :: Libraries",
+        "Topic :: System :: Networking",
+    ],
+)
